6550527ff47ceff06455d6764932486c571a7b53
crimson <crimsoncaelum@proton.me>
2024-10-01 16:45:55 +0100

    Edited Prelude.rs

--- iris-api/src/prelude.rs
+++ iris-api/src/prelude.rs
@@ -4,8 +4,8 @@ pub use crate::error::Error;
 // Alias Result to be the crate Result.
 pub type Result<T> = core::result::Result<T, Error>;
 
-// Generic Wrapper tuple struct for newtype pattern
+// Generic Wrapper tuple struct for newtype pattern.
 pub struct W<T>(pub T);
 
+// Assigns Format as f.
 pub use std::format as f;
-

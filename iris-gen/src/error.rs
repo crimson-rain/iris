c36959ce8660d5f733d3d7a97fb7871df865b2a1
Crimson-Rain <crimsoncaelum@proton.me>
2025-01-15 00:17:55 +0000

    Updated Entities

--- iris-gen/src/error.rs
+++ iris-gen/src/error.rs
@@ -1,6 +1,25 @@
+/* FILENAME: error.rs
+ * 
+ * DESCRIPTION 
+ * This file defines error types, using thiserror library.
+ * This library allows us to avoid boiler plate code, 
+ * when defining a variety of errors which can occur in the code.
+ * 
+ * NOTES
+ * 
+ * AUTHOR:    Rezwan Rahman (RAH22529097)
+ * CREATED:   04/11/2024
+ * MODIFIED:  14/11/2024
+ */
+
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum IrisGenError {
     
+}
+
+#[cfg(test)]
+mod tests {
+
 }

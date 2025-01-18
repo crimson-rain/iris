7b0404f0fc55a4ed14e9a368608ab298454a04a8
Crimson-Rain <crimsoncaelum@proton.me>
2025-01-18 21:41:48 +0000

    Error: Godot Project Not Launching!

--- iris-gen/src/vec_store.rs
+++ iris-gen/src/vec_store.rs
@@ -1,4 +1,5 @@
-//! This module provides functionality for creating and managing NPC memories.
+//! The modules purpose is to provide a entry point into the vec_store for
+//! storage and reterieval, and RAG.
 
 // Test for the Library
 #[cfg(test)]

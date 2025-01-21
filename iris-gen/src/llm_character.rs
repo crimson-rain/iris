c24ed648308e6635843d9417d9fbdaf254e08896
Crimson-Rain <crimsoncaelum@proton.me>
2025-01-21 15:53:16 +0000

    Added Memory

--- iris-gen/src/llm_character.rs
+++ iris-gen/src/llm_character.rs
@@ -1,5 +1,5 @@
-//! This module is where the NPC is created. 
-//! Using Godot Binding we create a new NPC. 
+//! This module is where the NPC is created.
+//! Using Godot Binding we create a new NPC.
 #![deny(clippy::todo)]
 
 use godot::builtin::GString;
@@ -7,26 +7,31 @@ use godot::classes::{CharacterBody2D, ICharacterBody2D};
 use godot::obj::Base;
 use godot::prelude::{godot_api, GodotClass};
 
+use crate::memory::MemoryStore;
+
 #[derive(GodotClass)]
 #[class(base=CharacterBody2D)]
 struct LLMCharacter {
-    base: Base<CharacterBody2D>,
     #[export]
     id: GString,
     #[export]
     profession: GString,
     #[export(multiline)]
     description: GString,
+
+    base: Base<CharacterBody2D>,
+    memory_store: MemoryStore
 }
 
 #[godot_api]
 impl ICharacterBody2D for LLMCharacter {
     fn init(base: Base<CharacterBody2D>) -> Self {
         LLMCharacter {
-            base,
             id: GString::new(),
             profession: GString::new(),
             description: GString::new(),
+            base,
+            memory_store: MemoryStore::default()
         }
     }
 

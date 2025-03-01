23e5e7e14911f9d57a429622c4621beea3dee45e
crimson-rain <crimsoncaelum@proton.me>
2025-03-01 02:27:35 +0000

    Commented iris_ecs, iris_gen and iris_godot and gave purpose of each library

--- crates/iris_godot/src/lib.rs
+++ crates/iris_godot/src/lib.rs
@@ -1,3 +1,10 @@
+//! iris_godot
+//! 
+//! `iris_godot` is the core and heart of this project allowing us to interact with the Godot Game Engine
+//! using FFIs and Bindings through the godot-rust crate. 
+//! 
+//! This library is responsible for connecting all the components together and have these components accessible in godot.
+
 use godot::prelude::*;
 
 struct Iris;

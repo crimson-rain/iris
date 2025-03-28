820a20d04e3e73cb5a5ba71664d6a9c6a01b6f80
crimson-rain <crimsoncaelum@proton.me>
2025-03-28 12:24:44 +0000

    Update to RAG

--- crates/iris_gen/src/rag/mod.rs
+++ crates/iris_gen/src/rag/mod.rs
@@ -7,8 +7,10 @@
 
 use qdrant_client::Qdrant;
 use qdrant_client::qdrant::{
-    CreateCollectionBuilder, PointStruct, UpsertPointsBuilder, VectorParamsBuilder,
+    CreateCollection, CreateCollectionBuilder, Distance, PointStruct, UpsertPointsBuilder, VectorParamsBuilder
 };
+use tokio::fs;
+use tokio::io::AsyncReadExt;
 
 use crate::error::IrisGenError;
 
@@ -64,8 +66,55 @@ async fn add_vectors() -> Result<(), IrisGenError> {
         .upsert_points(UpsertPointsBuilder::new("test_collection", points))
         .await?;
 
+
+
     dbg!(response);
     Ok(())
+}
+
+async fn collection_info() -> Result<(), IrisGenError> {
+    let client = Qdrant::from_url("http://localhost:6334")
+        .skip_compatibility_check()
+        .build()?;
+
+    let collection_list = client.list_collections().await?;
+    
+    dbg!(collection_list);
+
+    Ok(())
+}
+
+async fn create_npc_collection() -> Result<(), IrisGenError> {
+    let client = Qdrant::from_url("http://localhost:6334")
+        .skip_compatibility_check()
+        .build()?;
+
+    if client.collection_exists("npc_collection").await? {
+        return Ok(())
+    }
+
+
+
+    Ok(())
+} 
+
+
+async fn load_npc_data(folder: &str) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
+    let mut npc_data = Vec::new();
+    let mut dir = fs::read_dir(folder).await?;
+
+    while let Some(entry) = dir.next_entry().await? {
+        let path = entry.path();
+        if path.is_file() {
+            let mut file = fs::File::open(&path).await?;
+            let mut content = String::new();
+            file.read_to_string(&mut content).await?;
+            let filename = path.file_name().unwrap().to_string_lossy().to_string();
+            npc_data.push((filename, content));
+        }
+    }
+
+    Ok(npc_data)
 }
 
 #[cfg(test)]
@@ -80,5 +129,10 @@ mod tests {
     #[tokio::test]
     async fn test_collection() {
         assert!(create_collection().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn list_collections() {
+        assert!(collection_info().await.is_ok());
     }
 }

a0ccc2f6b773b75c7d757c2bcbd79f5abfb08058
crimson-rain <crimsoncaelum@proton.me>
2025-04-18 18:47:09 +0100

    Fixed Memory Issue in Iris Gen

--- crates/iris_gen/src/agent/maestro.rs
+++ crates/iris_gen/src/agent/maestro.rs
@@ -2,7 +2,6 @@
 //! Maestro is responsible for making LLM calls as well as handling various other
 //! tasks, is required to create the needed operation.
 
-use super::configs::DIALOGUE_SYSTEM_PROMPT;
 use super::model::Model;
 use crate::error::IrisGenError;
 use crate::rag::RAG;
@@ -11,61 +10,54 @@ use ollama_rs::generation::chat::ChatMessage;
 #[derive(Clone)]
 pub struct Maestro {
     model: Model,
-    history: Vec<ChatMessage>,
 }
 
 impl Default for Maestro {
     fn default() -> Self {
-        let system_message = ChatMessage::new(
-            ollama_rs::generation::chat::MessageRole::System,
-            DIALOGUE_SYSTEM_PROMPT.to_string(),
-        );
-
         Self {
             model: Model::default(),
-            history: vec![system_message],
         }
     }
 }
 
 impl Maestro {
-    pub async fn conduct_dialogue_gen(&mut self, prompt: String) -> Result<String, IrisGenError> {
+    pub async fn conduct_dialogue_gen(&mut self, prompt: String, history: &mut Vec<ChatMessage>) -> Result<String, IrisGenError> {
         let rag_res = self.conduct_rag(&prompt).await?;
 
         let rag_inject_prompt = format!("CONTEXT: {}, PROMPT: {}", rag_res, prompt);
 
         let resp = self
             .model
-            .generate_request(&rag_inject_prompt, &mut self.history)
+            .generate_request(&rag_inject_prompt, history)
             .await?;
 
-        dbg!(&self.history);
+        dbg!(&history);
 
         Ok(resp.message.content)
     }
 
     //    pub async fn conduct_dialogue_gen_with_tools(&mut self, prompt: String) -> Result<String, IrisGenError> {
     //        let rag_res = self.conduct_rag(&prompt).await?;
-    //
+    
     //        let rag_inject_prompt = format!("CONTEXT: {}, PROMPT: {}", rag_res, prompt);
-    //
+    
     //        let resp = self
     //            .model
     //            .generate_request_with_tools(&rag_inject_prompt, self.history.clone())
     //            .await?;
-    //
+    
     //        self.history.push(ChatMessage::new(
     //            ollama_rs::generation::chat::MessageRole::User,
     //            prompt.clone(),
     //        ));
-    //
+    
     //        self.history.push(ChatMessage::new(
     //            ollama_rs::generation::chat::MessageRole::Assistant,
     //            resp.message.content.clone(),
     //        ));
-    //
+    
     //        dbg!(&self.history);
-    //
+    
     //        Ok(resp.message.content)
     //    }
 
@@ -100,15 +92,15 @@ mod tests {
     #[tokio::test]
     async fn test_conduct_dialogue_gen() {
         let mut maestro = Maestro::default();
+        let mut history = Vec::new();
 
         assert!(
             maestro
-                .conduct_dialogue_gen("How are you?, Mel".to_string())
+                .conduct_dialogue_gen("How are you?, Mel".to_string(), &mut history)
                 .await
                 .is_ok()
         );
 
-        dbg!(maestro.history);
     }
 
     #[tokio::test]
@@ -120,25 +112,23 @@ mod tests {
     #[tokio::test]
     async fn test_conduct_dialogue_gen_with_history() {
         let mut maestro = Maestro::default();
+        let mut history = Vec::new();
 
         maestro
-            .conduct_dialogue_gen("What is your name?".to_string())
+            .conduct_dialogue_gen("What is your name?".to_string(), &mut history)
             .await
             .unwrap();
 
         maestro
-            .conduct_dialogue_gen("And whats the weather like in Roehampton?".to_string())
+            .conduct_dialogue_gen("And whats the weather like in Roehampton?".to_string(), &mut history)
             .await
             .unwrap();
 
         maestro
-            .conduct_dialogue_gen("What was the place I asked about?".to_string())
+            .conduct_dialogue_gen("What was the place I asked about?".to_string(), &mut history)
             .await
             .unwrap();
 
-        assert!(maestro.history.len() > 3);
-        assert!(maestro.history[0].role == ollama_rs::generation::chat::MessageRole::System);
-        assert!(maestro.history[1].role == ollama_rs::generation::chat::MessageRole::User);
-        assert!(maestro.history[2].role == ollama_rs::generation::chat::MessageRole::Assistant);
+        assert!(history.len() > 3);
     }
 }

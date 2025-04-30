754ace947047d0fb209d12d2910479300de09e4b
crimson-rain <crimsoncaelum@proton.me>
2025-04-30 12:54:22 +0100

    Update

--- iris_game/entites/monsters/skeleton/skeleton.gd
+++ iris_game/entites/monsters/skeleton/skeleton.gd
@@ -1,1 +1,50 @@
-extends Node2D
+extends CharacterBody2D
+
+# Speed of the Character, When Moving
+const SPEED: float = 145.0
+# Acceleration of the Character, Increases the Speed with a Multiplier
+const ACCELERATION: float = 15.0
+
+# STOP DISTANCE
+@export var stop_distance: float = 100.0
+
+@export var health: int = 250
+@export var damage: int = 100
+
+# Animation Player
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Player
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+
+# Handles the Physics Process in the Game, Such as Movement Etc.
+func _physics_process(_delta: float) -> void:
+	movement()
+
+# Handles the Movement of the Player Currently Manages 4-D Movement
+func movement() -> void:
+	var current_distance: float = global_position.distance_to(player.global_position)
+	
+	if current_distance > stop_distance:
+		var direction: Vector2 = global_position.direction_to(player.global_position)
+		velocity = direction * SPEED
+		update_animation(direction)
+	else:
+		velocity = Vector2.ZERO
+		update_animation(Vector2.ZERO)
+	
+	move_and_slide()
+
+# Update the Animation Based on the Movement of the Player
+func update_animation(direction: Vector2) -> void:
+	if direction.x < 0: 
+		animation_player.play("move_left")
+	elif direction.x > 0:
+		animation_player.play("move_right")
+	elif direction.y < 0:
+		animation_player.play("move_up")
+	elif direction.y > 0:
+		animation_player.play("move_down")
+	else:
+		animation_player.seek(0.35, true)
+		animation_player.pause()
--- iris_game/entites/npcs/npc.gd
+++ iris_game/entites/npcs/npc.gd
@@ -4,14 +4,29 @@
 
 extends NPC
 
+# STOP DISTANCE
+@export var stop_distance: float = 75.0
+
 @onready var iris: Iris = $Iris
 @onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Speed of the Character, When Moving
+const SPEED: float = 145.0
+# Acceleration of the Character, Increases the Speed with a Multiplier
+const ACCELERATION: float = 15.0
 
 var is_interacting: bool = false
+var follow_player: bool = false
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
-	
+
+func _physics_process(_delta: float) -> void:
+	if follow_player:
+		start_follow_player()
+
 func _on_interact(prompt: String) -> void:
 	if DialogueManager.is_dialogue_active or DialogueManager.chat_box_active:
 		return
@@ -24,13 +39,44 @@ func _on_interact(prompt: String) -> void:
 	iris.generate_dialogue(prompt, self.get_npc_info())
 
 func _on_iris_dialogue_generated(response: String) -> void:
-	
 	if not is_interacting:
 		return
 	
 	DialogueManager.start_dialogue(self.position, [response])
 
+func start_follow_player() -> void:
+	var current_distance: float = global_position.distance_to(player.global_position)
+	
+	if current_distance > stop_distance:
+		var direction: Vector2 = global_position.direction_to(player.global_position)
+		velocity = direction * SPEED
+		update_animation(direction)
+	else:
+		velocity = Vector2.ZERO
+		update_animation(Vector2.ZERO)
+	
+	move_and_slide()
+
+func update_animation(direction: Vector2) -> void:
+	if direction.x < 0: 
+		animation_player.play("move_left")
+	elif direction.x > 0:
+		animation_player.play("move_right")
+	elif direction.y < 0:
+		animation_player.play("move_up")
+	elif direction.y > 0:
+		animation_player.play("move_down")
+	else:
+		animation_player.seek(0.35, true)
+		animation_player.pause()
+
 func _handle_user_response(response: String) -> void:
+	if response == "follow":
+		follow_player = true
+		return
+	elif response == "stay":
+		follow_player = false
+		return
 	
 	if response.is_empty():
 		_end_interaction()
